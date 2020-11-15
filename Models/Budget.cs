b2c0292dc0a442b0c992fa7a139e5aac8d59f46e
Sebastian Casvean <iliecasv@yahoo.com>
2020-11-15 23:20:09 +0200

    -add swagger in project

---
+++ Models/Budget.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Casvean_IlieSebastian_Lab5.Models
+{
+    public class Budget
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool isApproved { get; set; }
+
+    }
+}
--- Models/ExpenseContext.cs
+++ Models/ExpenseContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Casvean_IlieSebastian_Lab5.Models;
 
 namespace Casvean_IlieSebastian_Lab5.Models
 {
@@ -14,5 +15,7 @@ namespace Casvean_IlieSebastian_Lab5.Models
         public DbSet<Expense> Expense { get; set; }
 
         public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+
+        public DbSet<Budget> Budget { get; set; }
     }
 }
